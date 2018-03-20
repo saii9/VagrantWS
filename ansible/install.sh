cf52fbbad9543230a7fb3f6d9bb2c3bf1fa473cd
Sai Innamuri <sundeepi9@yahoo.in>
2018-03-20 10:53:33 -0400

    fixing install.sh

--- ansible/install.sh
+++ ansible/install.sh
@@ -8,5 +8,5 @@ chmod 700 /home/vagrant/machines/simbuild.box/virtualbox/private_key
 
 cat << SET >> /etc/ansible/hosts
 [target] 
-target ansible_connection=ssh ansible_user=vagrant ansible_pass=vagrant ansible_ssh_private_key_file=/home/vagrant/machines/simbuild.box/virtualbox/private_key
+target ansible_connection=ssh ansible_user=vagrant ansible_pass=vagrant ansible_ssh_private_key_file=/home/vagrant/machines/Ansible.target/virtualbox/private_key
 SET
