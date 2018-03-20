a9e613f9b169689d45a17a739717b31525106b85
Sai Innamuri <sundeepi9@yahoo.in>
2018-03-20 11:25:13 -0400

    checking with differnt scritps to add ansible key

--- ansible/install.sh
+++ ansible/install.sh
@@ -4,7 +4,7 @@ yum install -y epel-release
 yum install -y ansible
 
 cp -r /home/vagrant/sync/.vagrant/machines /home/vagrant
-chmod 700 /home/vagrant/machines/simbuild.box/virtualbox/private_key 
+chmod 700 /home/vagrant/machines/Ansible.target/virtualbox/private_key 
 
 cat << SET >> /etc/ansible/hosts
 [target] 
