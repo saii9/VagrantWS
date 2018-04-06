942d9d4aacf34a9c33203b7cc4fdaf97c2b6ddc6
Sai Innamuri <sundeepi9@yahoo.in>
2018-04-06 16:34:27 -0400

    cross tool changes

---
+++ crosstool/gdbserver.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+checking whether to enable maintainer-specific portions of Makefiles... no
+checking for arm-unknown-linux-gnueabi-gcc... arm-unknown-linux-gnueabi-gcc
+checking for C compiler default output file name... a.out
+checking whether the C compiler works... yes
+checking whether we are cross compiling... yes
+checking for suffix of executables... 
+checking for suffix of object files... o
+checking whether we are using the GNU C compiler... yes
+checking whether arm-unknown-linux-gnueabi-gcc accepts -g... yes
+checking for arm-unknown-linux-gnueabi-gcc option to accept ISO C89... none needed
+checking for arm-unknown-linux-gnueabi-g++... arm-unknown-linux-gnueabi-g++
+checking whether we are using the GNU C++ compiler... yes
+checking whether arm-unknown-linux-gnueabi-g++ accepts -g... yes
+checking how to run the C preprocessor... arm-unknown-linux-gnueabi-gcc -E
+checking for grep that handles long lines and -e... /bin/grep
+checking for egrep... /bin/grep -E
+checking for ANSI C header files... yes
+checking for sys/types.h... yes
+checking for sys/stat.h... yes
+checking for stdlib.h... yes
+checking for string.h... yes
+checking for memory.h... yes
+checking for strings.h... yes
+checking for inttypes.h... yes
+checking for stdint.h... yes
+checking for unistd.h... yes
+checking minix/config.h usability... no
+checking minix/config.h presence... no
+checking for minix/config.h... no
+checking whether it is safe to define __EXTENSIONS__... yes
+checking for special C compiler options needed for large files... no
+checking for _FILE_OFFSET_BITS value needed for large files... 64
+checking build system type... x86_64-pc-linux-gnu
+checking host system type... arm-unknown-linux-gnueabi
+checking target system type... arm-unknown-linux-gnueabi
+checking for a BSD-compatible install... /usr/bin/install -c
+checking for arm-unknown-linux-gnueabi-ar... arm-unknown-linux-gnueabi-ar
+checking for arm-unknown-linux-gnueabi-ranlib... arm-unknown-linux-gnueabi-ranlib
+checking whether arm-unknown-linux-gnueabi-g++ supports C++11 features by default... yes
+checking for ANSI C header files... (cached) yes
+checking for main in -lmcheck... yes
+checking dependency style of arm-unknown-linux-gnueabi-gcc... gcc3
+=== configuring in build-gnulib-gdbserver (/home/vagrant/local/binutils-gdb/gdb/gdbserver/build/build-gnulib-gdbserver)
+configure: running /bin/bash ../../../gnulib/configure --disable-option-checking '--prefix=/home/vagrant/gdb'  '--host=arm-unknown-linux-gnueabi' 'host_alias=arm-unknown-linux-gnueabi' --cache-file=/dev/null --srcdir=../../../gnulib
+checking whether to enable maintainer-specific portions of Makefiles... no
+checking for arm-unknown-linux-gnueabi-gcc... arm-unknown-linux-gnueabi-gcc
+checking for C compiler default output file name... a.out
+checking whether the C compiler works... yes
+checking whether we are cross compiling... yes
+checking for suffix of executables... 
+checking for suffix of object files... o
+checking whether we are using the GNU C compiler... yes
+checking whether arm-unknown-linux-gnueabi-gcc accepts -g... yes
+checking for arm-unknown-linux-gnueabi-gcc option to accept ISO C89... none needed
+checking how to run the C preprocessor... arm-unknown-linux-gnueabi-gcc -E
+checking for grep that handles long lines and -e... /bin/grep
+checking for egrep... /bin/grep -E
+checking for ANSI C header files... yes
+checking for sys/types.h... yes
+checking for sys/stat.h... yes
+checking for stdlib.h... yes
+checking for string.h... yes
+checking for memory.h... yes
+checking for strings.h... yes
+checking for inttypes.h... yes
+checking for stdint.h... yes
+checking for unistd.h... yes
+checking minix/config.h usability... no
+checking minix/config.h presence... 
