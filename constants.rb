c8891bd5017bccecbf9910dbe39122b7800bc40f
Sai Innamuri <sundeepi9@yahoo.in>
2019-02-13 12:01:12 -0500

    ansible test env

--- constants.rb
+++ constants.rb
@@ -2,4 +2,6 @@
 class Constants
 	PROJECT_ROOT = 'D:\\Other\\vagrantWS'
 	DEPLOY_SCRIPTS_SYNC = 'D:\\Primasolv\\deployscripts'
+	DEPLOY_SCRIPTS2_SYNC = 'D:\\Primasolv\\deployscripts2'
+	WORKSPACE = 'D:\\'
 end 
